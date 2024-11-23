af167fdee06fbf857a1b9f41d0fe6b8a995ca021
KushKK29 <kush282930@gmail.com>
2024-11-24 00:43:05 +0530

    wqswqswqs

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
-import Header from "./components/Header";
 import { Routes, Route } from "react-router-dom";
 import React, { useState, useEffect } from "react";
+import Header from "./components/Header";
 import Login from "./pages/Requests";
 import Dashboard from "./pages/Dashboard";
 import AdminPanel from "./pages/AdminPanel";
 import Users from "./pages/Users";
 import AdminLogin from "./pages/AdminLogin";
+import RequestPanel from "./pages/RequestPanel";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import RequestPanel from "./pages/RequestPanel";
+
 function App() {
   const admin = [
     {
@@ -16,6 +17,7 @@ function App() {
       password: "admin@123",
     },
   ];
+
   const [login, setLogin] = useState(
     () => JSON.parse(localStorage.getItem("login")) || false
   );
@@ -25,13 +27,21 @@ function App() {
   const [roles, setRoles] = useState(
     () => JSON.parse(localStorage.getItem("roles")) || []
   );
-  const [requests, setRequests] = useState([
-    { id: 1, username: "Alice", email: "alice@example.com", role: "Admin" },
-    { id: 2, username: "Bob", email: "bob@example.com", role: "Moderator" },
-    { id: 3, username: "Charlie", email: "charlie@example.com", role: "User" },
-  ]);
+  const [requests, setRequests] = useState(
+    () =>
+      JSON.parse(localStorage.getItem("requests")) || [
+        { id: 1, username: "Alice", email: "alice@example.com", role: "Admin" },
+        { id: 2, username: "Bob", email: "bob@example.com", role: "Moderator" },
+        {
+          id: 3,
+          username: "Charlie",
+          email: "charlie@example.com",
+          role: "User",
+        },
+      ]
+  );
 
-  // Save to localStorage whenever login, users, or roles change
+  // Save to localStorage whenever login, users, roles, or requests change
   useEffect(() => {
     localStorage.setItem("login", JSON.stringify(login));
   }, [login]);
@@ -43,12 +53,17 @@ function App() {
   useEffect(() => {
     localStorage.setItem("roles", JSON.stringify(roles));
   }, [roles]);
+
+  useEffect(() => {
+    localStorage.setItem("requests", JSON.stringify(requests));
+  }, [requests]);
+
   return (
     <>
       <Header login={login} setLogin={setLogin} />
       <Routes>
         <Route
-          path="/"
+          path="/RBAC-Tailwind/"
           element={
             <AdminLogin admin={admin} login={login} setLogin={setLogin} />
           }
